1afcc53835a4be88fa2d365b1d5047edcce48676
MikolaChudinov <154053972+MikolaChudinov@users.noreply.github.com>
2025-02-26 19:02:48 +0300

    Добавлен app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+  console.log('🚀 Server запущен на http://localhost:3000');
+}
+
+bootstrap();
+
